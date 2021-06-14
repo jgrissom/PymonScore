bb8d83b2d25e09b28c3bbf34eebaa3107ede38ba
jgrissom <jgrissom91@gmail.com>
2021-06-14 09:54:00 -0500

    document top scores httpget method

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -31,7 +31,7 @@ namespace WordApi.Controllers
         {
             return _dataContext.Scores.FirstOrDefault(s => s.Id == id);
         }
-        [HttpGet("top/{num}"), ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet("top/{num}")]//, ApiExplorerSettings(IgnoreApi = true)]
         public IEnumerable<Score> GetTop(int num)
         {
             return _dataContext.Scores.OrderByDescending(s => s.Total).Take(num).ToArray();
