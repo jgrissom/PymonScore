b475b1aae1764c86c70b4e9884b957e9d3d7f99e
jgrissom <jgrissom91@gmail.com>
2021-06-16 22:41:38 -0500

    fix display-table

--- wwwroot/client.js
+++ wwwroot/client.js
@@ -23,23 +23,24 @@ function getData(url) {
 
 function showTable(scores) {
     const rows = document.querySelectorAll('tbody tr');
+    //rows.innerHTML = "";
     for (let i = 0; i < scores.length; i++){
-        if (scores[i].id != rows[i].id) {
-            rows[i].classList.add('highlight');
+        //if (scores[i].id != rows[i].id) {
+            // rows[i].classList.add('highlight');
             const cols = rows[i].querySelectorAll('td');
             cols[0].innerHTML = scores[i].name;
             cols[1].innerHTML = formatDate(new Date(scores[i].date));
             cols[2].innerHTML = scores[i].total;
             rows[i].id = scores[i].id;
-        }        
+        //}        
     }
     for (let i = scores.length; i < rows.length; i++){
         const cols = rows[i].querySelectorAll('td');
         cols.forEach(col => { col.innerHTML = ""; });
     }
-    document.querySelectorAll('.highlight').forEach(item => {
-        item.addEventListener('animationend', () => { item.classList.remove('highlight'); });
-    });
+    // document.querySelectorAll('.highlight').forEach(item => {
+    //     item.addEventListener('animationend', () => { item.classList.remove('highlight'); });
+    // });
 }
 
 function formatDate(date) {
@@ -58,11 +59,17 @@ connection.start().then(function () {
 });
 
 // upon receipt of add message, refresh table rows
-connection.on("ReceiveAddMessage", function (id) {
-    getData('api/score/top/10');
+connection.on("ReceiveAddMessage", function (scores) {
+    showTable(scores);
+    const element = document.getElementById('container')
+    element.classList.add('highlight-add');
+    element.addEventListener('animationend', () => { element.classList.remove('highlight-add'); });
 });
 
 // upon receipt of delete message, refresh table rows
-connection.on("ReceiveDeleteMessage", function (id) {
-    getData('api/score/top/10');
+connection.on("ReceiveDeleteMessage", function (scores) {
+    showTable(scores);
+    const element = document.getElementById('container')
+    element.classList.add('highlight-delete');
+    element.addEventListener('animationend', () => { element.classList.remove('highlight-delete'); });
 });
